fedcce29d41408687d836df947af89535f808c08
autofahrer <149318127+autofahrer@users.noreply.github.com>
2024-07-12 16:00:25 +0200

    add task view bottomSheetFragment finished without handing data over

--- app/src/main/java/io/github/secondbrainplanner/NewTaskSheet.kt
+++ app/src/main/java/io/github/secondbrainplanner/NewTaskSheet.kt
@@ -5,53 +5,38 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import io.github.secondbrainplanner.databinding.FragmentNewTaskSheetBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+class NewTaskSheet : BottomSheetDialogFragment()
+{
+   private lateinit var binding: FragmentNewTaskSheetBinding
+   private lateinit var taskViewModel: TaskViewModel
 
-/**
- * A simple [Fragment] subclass.
- * Use the [NewTaskSheet.newInstance] factory method to
- * create an instance of this fragment.
- */
-class NewTaskSheet : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val activity = requireActivity()
+        taskViewModel = ViewModelProvider(activity).get(TaskViewModel::class.java)
+        binding.newTaskAddButton.setOnClickListener {
+            saveAction()
         }
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_new_task_sheet, container, false)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = FragmentNewTaskSheetBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment NewTaskSheet.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-                NewTaskSheet().apply {
-                    arguments = Bundle().apply {
-                        putString(ARG_PARAM1, param1)
-                        putString(ARG_PARAM2, param2)
-                    }
-                }
+
+    private fun saveAction()
+    {
+        taskViewModel.name.value = binding.newTaskName.text.toString()
+        taskViewModel.description.value = binding.newTaskDescription.text.toString()
+        binding.newTaskName.setText("")
+        binding.newTaskDescription.setText("")
+        dismiss()
     }
+
+
 }
