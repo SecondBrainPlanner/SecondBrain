9d494044ebbd733846f2e0fa521685203601e9f1
Elias <150338005+Administrator25@users.noreply.github.com>
2024-07-15 11:42:40 +0200

    updated Taskklasse
    In der Taskklasse wurden die neuen Eigenschaften hinzugefügt

--- app/src/main/java/io/github/secondbrainplanner/Task.java
+++ app/src/main/java/io/github/secondbrainplanner/Task.java
@@ -1,19 +1,43 @@
 package io.github.secondbrainplanner;
 
 public class Task {
-    private String name;
+    private int id;
+    private String title;
     private String description;
+    private int created_at;
+    private int due_date;
+    private int completed;
+    private int completed_at;
+    private int updated_at;
 
-    public Task(String name, String description) {
-        this.name = name;
+    public Task(int id, String title, String description, int created_at, int due_date, int completed, int completed_at, int updated_at) {
+        this.title = title;
         this.description = description;
+        this.created_at = created_at;
+        this.due_date = due_date;
+        this.completed = completed;
+        this.completed_at = completed_at;
+        this.updated_at = updated_at;
     }
 
-    public String getName() {
-        return name;
+    public int getId() { return id; }
+
+    public String getTitle() {
+        return title;
     }
 
     public String getDescription() {
         return description;
     }
+
+    public int getCreated_at() { return created_at; }
+
+    public int getDue_date() { return due_date; }
+
+    public int getCompleted() { return completed; }
+
+    public int getCompleted_at() { return completed_at; }
+
+    public int getUpdated_at() { return updated_at; }
+
 }
