f0c3f61579bc5255ac9dde4d2dac86747281f3cd
autofahrer <149318127+autofahrer@users.noreply.github.com>
2024-07-17 11:40:20 +0200

    Main: text view days created / on scroll listener added main.xml: adjusted for top bar
    Main.java:
    text view days linked to xml
    
    on scroll listener gets updated top position and calls highlighting method to update highlight constantly
    
    main.xml:
    added week day layout with all the week days
    
    set recycler view under the week day layout

--- app/src/main/java/io/github/secondbrainplanner/MainActivity.java
+++ app/src/main/java/io/github/secondbrainplanner/MainActivity.java
@@ -1,6 +1,7 @@
 package io.github.secondbrainplanner;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -21,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
     private TaskViewModel taskViewModel;
     private TaskAdapter taskAdapter;
+    private TextView textViewMon, textViewTue, textViewWed, textViewThu, textViewFri, textViewSat, textViewSun;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,12 +34,22 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        textViewMon = findViewById(R.id.textViewMon);
+        textViewTue = findViewById(R.id.textViewTue);
+        textViewWed = findViewById(R.id.textViewWed);
+        textViewThu = findViewById(R.id.textViewThu);
+        textViewFri = findViewById(R.id.textViewFri);
+        textViewSat = findViewById(R.id.textViewSat);
+        textViewSun = findViewById(R.id.textViewSun);
+
         TaskViewModelFactory factory = new TaskViewModelFactory(getApplication());
         taskViewModel = new ViewModelProvider(this, factory).get(TaskViewModel.class);
 
         taskAdapter = new TaskAdapter(taskViewModel, getSupportFragmentManager());
         binding.recyclerView.setLayoutManager(new LinearLayoutManager(this));
         binding.recyclerView.setAdapter(taskAdapter);
+
+        taskViewModel.items.observe(this, items -> taskAdapter.setItems(items));
 
         taskViewModel.items.observe(this, new Observer<List<Object>>() {
             @Override
@@ -48,6 +60,14 @@ public class MainActivity extends AppCompatActivity {
 
         binding.newTaskButton.setOnClickListener(view -> {
             new NewTaskSheet().show(getSupportFragmentManager(), "newTaskTag");
+        });
+
+        binding.recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+                updateHighlightedWeekDay();
+            }
         });
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
