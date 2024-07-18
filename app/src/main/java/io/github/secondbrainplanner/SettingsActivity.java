b46375f826dafc7b16acbb446f3bbcc3e69394db
Elias <150338005+Administrator25@users.noreply.github.com>
2024-07-18 14:22:15 +0200

    Update SettingsActivity.java
    Es gibt nun ein Item für "Datenbank exportieren"

--- app/src/main/java/io/github/secondbrainplanner/SettingsActivity.java
+++ app/src/main/java/io/github/secondbrainplanner/SettingsActivity.java
@@ -1,6 +1,8 @@
 package io.github.secondbrainplanner;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +10,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+
 public class SettingsActivity extends AppCompatActivity {
+
+    private ListView listView;
+    private ArrayAdapter<String> adapter;
+    private ArrayList<String> settingsList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +28,13 @@ public class SettingsActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        listView = findViewById(R.id.listViewSettings);
+        settingsList = new ArrayList<>();
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, settingsList);
+        listView.setAdapter(adapter);
+
+        settingsList.add("Datenbank exportieren");
+
     }
 }
