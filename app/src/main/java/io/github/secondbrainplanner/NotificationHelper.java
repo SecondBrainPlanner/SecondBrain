79785d86123d231b1d5308961d083167e614aed4
Elias <150338005+Administrator25@users.noreply.github.com>
2024-07-23 12:26:05 +0200

    added notification logic
    Über den AlarmHelper können jetzt Notifications erstellt werden

---
+++ app/src/main/java/io/github/secondbrainplanner/NotificationHelper.java
@@ -0,0 +1,33 @@
+package io.github.secondbrainplanner;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.os.Build;
+
+import androidx.core.app.NotificationCompat;
+
+public class NotificationHelper {
+    private static final String CHANNEL_ID = "secondbrain";
+    private static final String CHANNEL_NAME = "SecondBrain";
+    private static final String CHANNEL_DESCRIPTION = "Benachrichtigungen für Tasks";
+
+    public static void createNotification(Context context, String title, String message, int id) {
+        NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, CHANNEL_NAME, NotificationManager.IMPORTANCE_DEFAULT);
+            channel.setDescription(CHANNEL_DESCRIPTION);
+            notificationManager.createNotificationChannel(channel);
+        }
+
+        Notification notification = new NotificationCompat.Builder(context, CHANNEL_ID)
+                .setContentTitle(title)
+                .setContentText(message)
+                .setSmallIcon(R.drawable.notification)
+                .build();
+
+        notificationManager.notify(id, notification);
+    }
+}
